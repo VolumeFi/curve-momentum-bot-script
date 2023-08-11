376afb93280189fa55493649d593e507fd83a7f1
webelf101 <m63974529@gmail.com>
2023-08-12 03:30:38 +0800

    add index

---
+++ index.js
@@ -0,0 +1,21 @@
+const Sentry = require('@sentry/node');
+const express = require('express');
+const { processDeposits } = require('./momentum');
+
+require('dotenv').config();
+
+processDeposits();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Service is running!');
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Check status on port ${port}`);
+});
+
